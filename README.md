b3922b6b252b989a622b2bb597f7d6ebe4323385
Abhinav Prakash <abhinavp@tamu.edu>
2020-06-05 01:58:56 -0500

    README and example file added

---
+++ README.md
@@ -0,0 +1,18 @@
+# Function comparison using Gaussian process (funGP)
+
+## Description:
+This repository contains the matlab code to implement funGP algorithm given 
+in *Prakash, A., Tuo, R., & Ding, Y. (2020). "Gaussian process aided function comparison using noisy scattered data". arXiv preprint arXiv:2003.0789* available at this [link](https://arxiv.org/abs/2003.07899).
+
+## Usage:
+
+- The main function file is `funGP.m`. The details about the input and the output of this function is given in the file.
+
+- An example file is available (`example.m`) that applies the algorithm on a small sized problem. 
+
+## Support for other languages:
+An R implementation of the same algorithm can be found in `DSWE` package available on `Github` at https://github.com/TAMU-AML/DSWE-Package. 
+
+
+
+
---
+++ example.m
@@ -0,0 +1,107 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%    MIT License
+%  
+%    Copyright (c) 2020 Abhinav Prakash
+%  
+%    Permission is hereby granted, free of charge, to any person obtaining a copy
+%    of this software and associated documentation files (the "Software"), to deal
+%    in the Software without restriction, including without limitation the rights
+%    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+%    copies of the Software, and to permit persons to whom the Software is
+%    furnished to do so, subject to the following conditions:
+%  
+%    The above copyright notice and this permission notice shall be included in all
+%    copies or substantial portions of the Software.
+%  
+%    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+%    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+%    SOFTWARE.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   AUTHOR: ABHINAV PRAKASH
+%   DESCRIPTION:
+%   An example file for implementing funGP algorithm for the following two cases:
+%   1) function f1(x) = sin(2*pi*x) and f2(x) = 0.8*sin(2*pi*x)
+%   2) both f1(x) and f2(x) are same as sin(2*pi*x)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%Example 1: when f1(x) and f2(x) are different
+
+rng(1); %setting random seed
+
+x1 = rand(100,1); %random input points for dataset1
+y1 = sin(2*pi*x1) + normrnd(0,0.1,100,1); %generating response for dataset1 by adding noise to f1(x)
+
+x2 = rand(100,1); %random input points for dataset2
+y2 = 0.8*sin(2*pi*x2) + normrnd(0,0.1,100,1); %generating response for dataset2 by adding noise to f2(x)
+
+%plotting the data
+scatter(x1,y1);
+hold on;
+scatter(x2,y2);
+hold off;
+
+confLevel = 0.95; %setting confidence level
+xtest = linspace(0,1,100)'; %regular 1-dim grid of 100 points
+
+%running the algorithm
+out = funGP(x1, y1, x2, y2, xtest, confLevel);
+
+%printing out the results
+if out.differ == true
+    fprintf("The functions are statistically different.\n");
+    fprintf("Number of points for which the functions are different are %d out of %d test points.\n",out.nPoints,size(xtest,1));
+else
+    fprintf("The functions are statistically the same.\n");
+end
+
+%plotting the difference along with confidence band
+plot(xtest,out.muDiff,'LineWidth',2,'Color','blue');
+hold on;
+plot(xtest, out.band(:,1),'--','LineWidth',2,'Color','red');
+plot(xtest, out.band(:,2),'--','LineWidth',2,'Color','red');
+hold off;
+
+
+%%Example 2: when f1(x) and f2(x) are the same
+
+rng(2); %setting random seed
+
+x1 = rand(100,1); %random input points for dataset1
+y1 = sin(2*pi*x1) + normrnd(0,0.1,100,1); %generating response for dataset1 by adding noise to f1(x)
+
+x2 = rand(100,1); %random input points for dataset2
+y2 = sin(2*pi*x2) + normrnd(0,0.1,100,1); %generating response for dataset2 by adding noise to f2(x)
+
+%plotting the data
+scatter(x1,y1);
+hold on;
+scatter(x2,y2);
+hold off;
+
+confLevel = 0.95; %setting confidence level
+xtest = linspace(0,1,100)'; %regular 1-dim grid of 100 points
+
+%running the algorithm
+out = funGP(x1, y1, x2, y2, xtest, confLevel);
+
+%printing out the results
+if out.differ == true
+    fprintf("The functions are statistically different.\n");
+    fprintf("Number of points for which the functions are different are %d out of %d test points.\n",out.nPoints,size(xtest,1));
+else
+    fprintf("The functions are statistically the same.\n");
+end
+
+%plotting the difference along with confidence band
+plot(xtest,out.muDiff,'LineWidth',2,'Color','blue');
+hold on;
+plot(xtest, out.band(:,1),'--','LineWidth',2,'Color','red');
+plot(xtest, out.band(:,2),'--','LineWidth',2,'Color','red');
+hold off;
+
+
